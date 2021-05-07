6305558980c16a25ab0d8867087773bd5a3212b9
Alina Savin <alinasavin@Alinas-MacBook-Pro.local>
2021-05-07 17:48:44 +0100

    fetch-request-done-and-diplayed

--- main.js
+++ main.js
@@ -8,7 +8,27 @@ function handleFoodChange() {
   foodToSearch = document.querySelector("#food-input").value;
 }
 
+const YOUR_APP_ID = "adc26e15"
+const YOUR_APP_KEY = "3dc4d461a603a76bb8d7d688f5a8c5fb"
+
+
+const aTag = document.getElementById("recipe-label");
 async function fetchRecipe(food) {
   //--- write your code below ---
   //--- write your code above ---
+  // use fetch to do GET request for recipe
+  const requestUrl = await fetch(`https://api.edamam.com/search?q=${food}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`);
+  //console.log(requestUrl);
+  const dataResponse = await requestUrl.json();
+
+  console.log(dataResponse);
+  // grab the first recipe in the hits array
+  const dataResult = dataResponse.hits[0];
+  console.log(dataResult);
+  // replace the innerHTML with the label name of the recipe
+  aTag.innerHTML = dataResult.recipe.label;
+  // set the href attribute to be the recipe's url
+  aTag.href = dataResult.recipe.url;
 }
+
+
